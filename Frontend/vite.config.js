db6d26afb9fcca406eed1700aafa39e36b055f29
krishna jeena02 <krishjeena8700745775@gmail.com>
2026-03-13 11:46:12 +0530

    change content

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
